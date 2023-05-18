4856ad412176db481fe0e82351ed94c5ed3214ce
Elif <eliiftum@gmail.com>
2023-05-18 13:36:55 +0300

    first commit

---
+++ Pettition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pettition
+//
+//  Created by Elif Tum on 18.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
